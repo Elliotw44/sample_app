f06468d012e78c06fe0dfe167cfe274703d4095a
Elliow44 <Elliotrex2@hotmail.com>
2013-05-29 14:29:40 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+    factory :user do
+        name   "Elliot Weil"
+        email  "weil@example.com"
+        password "hola123"
+        password_confirmation "hola123"
+    end
+end
+
